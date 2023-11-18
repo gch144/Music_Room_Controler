f9305c7e7bd7801187ffc30d9eac8b4a54e50eba
Gaurav Chauhan <gauravchauhan8600@gmail.com>
2023-11-19 00:01:38 +0100

    update version

--- api/serializers.py
+++ api/serializers.py
@@ -7,3 +7,17 @@ class RoomSerializer(serializers.ModelSerializer):
         model = Room
         fields = ('id', 'code', 'host', 'guest_can_pause',
                   'votes_to_skip', 'created_at')
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
+
+
+class UpdateRoomSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(validators=[])
+
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip', 'code')
